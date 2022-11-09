71da749df1d031029794bb8d94bec8fd8168a75e
Karl Thomas <thokrl@amazon.com>
2022-11-08 20:12:24 -0500

    Removed PDF crawling.  It depended on an unpatched version of pdfreader

--- src/layer/lib/crawler.js
+++ src/layer/lib/crawler.js
@@ -4,7 +4,6 @@
 const chromium = require("chrome-aws-lambda");
 const fs = require('fs')
 const https = require("https");
-const pdfreader = require("pdfreader");
 const crypto = require("crypto");
 
 
@@ -33,74 +32,7 @@ async function getPage(url) {
   }
 }
 
-async function getTextFromPDF(url) {
-    var pdfBuffer = await bufferize(url);
-    var pdfParagraphs = await readlines(pdfBuffer);
-    var allLines = pdfParagraphs.flat(5);
-    return allLines.join("\n");
-  }
-  
 
-async function bufferize(url) {
-  var hn = url.substring(url.search("//") + 2);
-  hn = hn.substring(0, hn.search("/"));
-  var pt = url.substring(url.search("//") + 2);
-  pt = pt.substring(pt.search("/"));
-  const options = { hostname: hn, port: 443, path: pt, method: "GET" };
-  return new Promise(function(resolve, reject) {
-    var buff = new Buffer.alloc(0);
-    const req = https.request(options, (res) => {
-      res.on("data", (d) => {
-        buff = Buffer.concat([buff, d]);
-      });
-      res.on("end", () => {
-        resolve(buff);
-      });
-    });
-    req.on("error", (e) => {
-      console.error("https request error: " + e);
-    });
-    req.end();
-  });
-}
-
-async function readlines(buffer, xwidth) {
-  return new Promise((resolve, reject) => {
-    var pdftxt = new Array();
-    var pg = 0;
-    new pdfreader.PdfReader().parseBuffer(buffer, function(err, item) {
-      if (err) console.log("pdf reader error: " + err);
-      else if (!item) {
-        pdftxt.forEach(function(a, idx) {
-          pdftxt[idx].forEach(function(v, i) {
-            pdftxt[idx][i].splice(1, 2);
-          });
-        });
-        resolve(pdftxt);
-      } else if (item && item.page) {
-        pg = item.page - 1;
-        pdftxt[pg] = [];
-      } else if (item.text) {
-        var t = 0;
-        var sp = "";
-        pdftxt[pg].forEach(function(val, idx) {
-          if (val[1] == item.y) {
-            if (xwidth && item.x - val[2] > xwidth) {
-              sp += " ";
-            } else {
-              sp = "";
-            }
-            pdftxt[pg][idx][0] += sp + item.text;
-            t = 1;
-          }
-        });
-        if (t == 0) {
-          pdftxt[pg].push([item.text, item.y, item.x]);
-        }
-      }
-    });
-  });
-}
 
 
 
@@ -128,9 +60,9 @@ module.exports = async function(urls,callback=undefined) {
         title = await page.title()
 
       } else {
-        console.log("Indexing PDF " + url);
-        title = url.split("/").slice(-1)[0]
-        content = await getTextFromPDF(url)
+
+        console.log("PDF crawling is no longer supported")
+        continue;
       }
       if(callback){
         callback({
